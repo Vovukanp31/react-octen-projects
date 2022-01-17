c08c780181d718f99e4214dc9a94376c9e337f93
Vovukanp31 <vpapeta31@gmail.com>
2022-01-17 15:39:10 +0200

    react

---
+++ src/services/userServices/UserServices.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+let axiosInstance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com',
+    headers: {}
+});
+
+const getUsers = () => {
+  return axiosInstance.get('/users');
+}
+
+export default {
+    getUsers
+}
