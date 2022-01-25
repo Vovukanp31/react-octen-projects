747a2285d82d962dff0e269281b10ea07ae4bff4
Vovukanp31 <vpapeta31@gmail.com>
2022-01-25 14:28:07 +0200

    react cw-3 in progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import css from './App.module.css';
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
+<Header/>
 
+      <div className={.mainPart}></div>
     </div>
   );
 }
