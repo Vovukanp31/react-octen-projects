eb912fc38f0f3e97868276543d7edbe5bdd932bd
Vovukanp31 <vpapeta31@gmail.com>
2022-02-07 16:47:27 +0200

    react hw-6 in progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import {useReducer} from "react";
+import {useForm} from "react-hook-form";
+
+// const reducer = (state, action) = {
+//
+// }
 
 function App() {
+  // const [state, dispatch] = useReducer(reducer, {cats:[], dogs:[]});
+
+  const addDog = (e) => {
+    e.preventDefault();
+
+  }
+
+  const addCat = (e) => {
+    e.preventDefault()
+  }
+
+  const {register, handleSubmit} = useForm()
   return (
     <div className="App">
+      <form>
+        <input type="text" {...register('cat')}/>
+        <button onClick={handleSubmit(addCat)}>add cat</button>
 
+        <input type="text" {...register('dog')}/>
+        <button onClick={handleSubmit(addDog)}>add dog</button>
+      </form>
     </div>
   );
 }
