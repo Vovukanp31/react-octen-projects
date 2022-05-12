f8f5b74b0c6d948385027fbc69b18198197c61c7
Vovukanp31 <vpapeta31@gmail.com>
2022-05-12 13:45:17 +0300

    life cicle of component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import {useReducer} from "react";
 
 
 function App() {
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'inc': {
+        return {...state, counter:state.counter + 1};
+      };
+      case 'dec': {
+        return {...state, counter:state.counter - 1};
+      }
+      default: return state;
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, {counter: 0})
+
   return (
     <div className="App">
-
+      {state.counter}
+      <button onClick={() => dispatch({type: 'inc'})}>inc</button>
+      <button onClick={() => dispatch({type: 'dec'})}>dec</button>
     </div>
   );
 }
